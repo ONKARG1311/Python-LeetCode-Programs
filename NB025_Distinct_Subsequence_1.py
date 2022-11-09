9456d847c5250dd397a4628c55f48ff8e312aa3b
ONKAR <onkarg1311@gmail.com>
2022-11-10 00:49:22 +0530

    commit

--- NB025_Distinct_Subsequence_1.py
+++ NB025_Distinct_Subsequence_1.py
@@ -3,13 +3,17 @@ from collections import defaultdict
 
 class Solution:
     def numDistinct(self, s: str, t: str) -> int:
-        c_indices = defaultdict(list)
+
+        index = defaultdict(list)
         max_counts = [1] + [0] * len(t)
+
         for i, c in enumerate(t):
-            c_indices[c].append(i + 1)
+            index[c].append(i + 1)
+
         for c in s:
-            for i in reversed(c_indices[c]):
+            for i in reversed(index[c]):
                 max_counts[i] += max_counts[i - 1]
+
         return max_counts[-1]
 
 
